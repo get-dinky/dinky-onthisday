d410d2fbedca8fc4cd427a9c4aa6bee613259cce
Julian Prester <hi@julianprester.com>
2024-02-02 11:59:16 +1100

    Add fullscreen panel

--- dinky_onthisday/__init__.py
+++ dinky_onthisday/__init__.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageFont, ImageDraw
 import pkg_resources
 from io import BytesIO
 
+from dinky.display_configuration import DisplayConfiguration
 from dinky.layouts.layout_configuration import Zone
 
 hookimpl = pluggy.HookimplMarker("dinky")
@@ -21,27 +22,42 @@ class DinkyOnThisDayPlugin:
         )
         return response.json()['selected']
 
-
     @hookimpl
-    def dinky_draw_zone(self, zone: Zone):
+    def dinky_draw(self, zone: Zone, fullscreen: DisplayConfiguration):
+        # Events
         events = self._get_events()
-        max_events = int(zone.height * zone.height / 15000)
-        if len(events) > max_events:
-            events = random.sample(events, max_events)
-        events = sorted(events, key=lambda event: event['year'])
-        text = "\n".join([f"{event['year']}: {event['text']}" for event in events])
-        wrapper = textwrap.TextWrapper(width=int(0.14 * (zone.width - 2 * zone.padding)), max_lines=3, subsequent_indent='    ')
-        text = "\n".join([wrapper.fill(f"{event['year']}: {event['text']}") for event in events])
-        im = Image.new('RGB', (zone.width, zone.height), (255, 255, 255))
-        draw = ImageDraw.Draw(im)
 
+        # Fonts
         font_data = pkg_resources.resource_stream('dinky_onthisday', 'fonts/Roboto-Regular.ttf')
         font_bytes = BytesIO(font_data.read())
         font_header = ImageFont.truetype(font_bytes, 36)
         font_bytes.seek(0)
         font_regular = ImageFont.truetype(font_bytes, 14)
 
+        # Panel
+        max_events = int(zone.height * zone.height / 15000)
+        if len(events) > max_events:
+            events_sample = random.sample(events, max_events)
+        events_sample = sorted(events_sample, key=lambda event: event['year'])
+        text = "\n".join([f"{event['year']}: {event['text']}" for event in events])
+        wrapper = textwrap.TextWrapper(width=int(0.14 * (zone.width - 2 * zone.padding)), max_lines=3, subsequent_indent='    ')
+        text = "\n".join([wrapper.fill(f"{event['year']}: {event['text']}") for event in events_sample])
+        im_panel = Image.new('RGB', (zone.width, zone.height), (255, 255, 255))
+        draw = ImageDraw.Draw(im_panel)
+
         draw.rectangle((zone.padding, zone.padding, zone.width-zone.padding, zone.padding + 55), fill=self.primary_color)
         draw.text((zone.padding + 5, zone.padding + 5), "On This Day", font=font_header, fill="white")
         draw.multiline_text((zone.padding + 5, zone.padding + 5 + 55), text, font=font_regular, fill="black")
-        return im
+
+        # Fullscreen
+        events = sorted(events, key=lambda event: event['year'])
+        text = "\n".join([f"{event['year']}: {event['text']}" for event in events])
+        wrapper = textwrap.TextWrapper(width=int(0.16 * (fullscreen.width - 2 * zone.padding)), subsequent_indent='    ')
+        text = "\n".join([wrapper.fill(f"{event['year']}: {event['text']}") for event in events])
+        im_fullscreen = Image.new('RGB', (fullscreen.width, fullscreen.height), (255, 255, 255))
+        draw = ImageDraw.Draw(im_fullscreen)
+        draw.rectangle((zone.padding, zone.padding, fullscreen.width-zone.padding, zone.padding + 55), fill=self.primary_color)
+        draw.text((zone.padding + 5, zone.padding + 5), "On This Day", font=font_header, fill="white")
+        draw.multiline_text((zone.padding + 5, zone.padding + 5 + 55), text, font=font_regular, fill="black")
+
+        return im_panel, im_fullscreen
